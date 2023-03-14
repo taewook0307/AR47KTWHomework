e7578896d1a90f1cb6a445f35add2c8c720236ee
TaeWook <ktwisland111@gmail.com>
2023-03-14 15:51:20 +0900

    김태욱 : 배열 printf 기초 const

--- KTW_Homework/013_Function/013_Function.cpp
+++ KTW_Homework/013_Function/013_Function.cpp
@@ -16,7 +16,7 @@ void    Function      ()
 //외부에 자신의 결과값을 받기위한 메모리 공간이 필요함을 알림
 
 //이름의 의미
-//해당 함수의 램에서의 메모리 위치
+//해당 함수의 램에서의 메모리 위치 주소값
 //변수의 이름의 의미와 동일
 
 //인자의 의미
@@ -29,24 +29,24 @@ void    Function      ()
 
 
 //함수를 실행하는데 필요한 메모리 비용 : 인자 전체 크기 + a
-int Plus(int _Left, int _Right)     //필요한 메모리 비용 : 8byte + a
+int Plus(int _Left, int _Right)     //필요한 메모리 비용 : 8byte(인자 _Left와 _Right의 byte값) + a
 //Plus라는 이름의 함수
 //지역변수로 _Left, _right를 가짐
-//해당 함수를 실행한 외부 메모리 영역에 4바이트 값을 전달
+//해당 함수를 실행한 외부 메모리 영역에 4byte 값을 전달
 {
     return _Left + _Right;
 }
 
-void Test(int _Value) {     //필요한 메모리 비용 : 4byte + a
+void Test(int _Value) {     //필요한 메모리 비용 : 4byte(인자 _Value의 byte값) + a
     _Value = 10;
     return;
 }
 
 int main()
 {
-    Plus(20, 30);   //4바이트를 메모리에서 사용한 것
+    Plus(20, 30);   //4byte를 메모리에서 사용한 것
 
     int ReturnResult = sizeof(Plus(20, 30));
     //void는 sizeof를 할 수 없음
-    //sizeof(Test(20)) <= 실행 불가능
+    //sizeof(Test(20)) <= 실행 불가능 : Test()함수가 void 타입이라서
 }
---
+++ KTW_Homework/017_Printf/017_Printf.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+//컨트롤 + k + c 내가 선택한 범위 주석
+
+int main()
+{
+    //콘솔창에 글자를 띄운다.
+    printf_s("aaaa");
+
+    char Arr[5] = { '1','2','3','4','5' };
+
+    //char Value1 = Arr; <= 오류 발생 : 자료형이 다르기 때문에
+
+    //제로 베이스 기반
+    //배열의 개수를 셀 때 0부터 세는 것을 제로베이스 기반 인덱스 표현
+    char Value1 = Arr[0]; //<= Value1 = '1';
+    char Value2 = Arr[1]; //<= Value2 = '2';
+    char Value3 = Arr[2]; //<= Value2 = '3';
+    char Value4 = Arr[3]; //<= Value2 = '4';
+    char Value5 = Arr[4]; //<= Value2 = '5';
+
+    //문자열의 끝을 표현하기 위해 뒤에 0을 붙여줌
+    //0이 없으면 문자열이 끝이라고 생각하지 않음
+    //printf_s는 0을 만날때까지를 계속 출력하는 함수
+    //printf_s(Arr);      //<= 12345 뒤에 이상한 값이 출력
+
+    char Arr1[5] = { '1','2','3','4', 0 /*'5'*/};
+
+    printf_s(Arr1);
+
+    //char Arr[5] = "12345" <= 뒤에 0이 자동으로 붙기 때문에 Arr[5] > Arr[6]으로 바꿔주어야 한다
+    char Arr2[6] = "12345";
+
+    printf_s(Arr2);
+}
