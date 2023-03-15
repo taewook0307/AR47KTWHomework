43be46542d7e41df57d52e82fea0b2cbdd3eafe7
TaeWook <ktwisland111@gmail.com>
2023-03-15 15:48:32 +0900

    김태욱 : 포인터 기초

---
+++ KTW_Homework/021_Pointer001/021_Pointer001.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+int main()
+{
+    {
+        int Value0 = 0;
+        int Value1 = 0;
+
+        //& : 특정 변수의 메모리 영역의 주소값을 끌어내는 것
+        int* ValuePtr0 = &Value0;
+        int* ValuePtr1 = &Value1;
+    }
+
+    {
+        int Arr[3] = { 0,1,2 };
+
+        int* ValuePtr0 = &Arr[0];
+        int* ValuePtr1 = &Arr[1];
+        int* ValuePtr2 = &Arr[2];
+
+        __int64 Address0 = 0;
+        __int64 Address1 = 0;
+        __int64 Address2 = 0;
+
+        Address0 = (__int64)ValuePtr0;
+        Address1 = (__int64)ValuePtr1;
+        Address2 = (__int64)ValuePtr2;
+    }
+}
---
+++ KTW_Homework/023_NullPtr/023_NullPtr.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+    int Value = 10;
+
+    //nullptr == 0
+    //주소를 가리키지 않는다, 아직 아무도 가리키고 잇지 않다.
+    int* Ptr = nullptr;
+
+    *Ptr = 200;
+
+    //int* Ptr = 0;     <= c스타일
+}
