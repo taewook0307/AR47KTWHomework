fae4f90c25c499c0e946a09020d80f850fcdb68f
TaeWook <ktwisland111@gmail.com>
2023-03-15 15:48:48 +0900

    김태욱 : 포인터 기초

--- KTW_Homework/023_NullPtr/023_NullPtr.cpp
+++ KTW_Homework/023_NullPtr/023_NullPtr.cpp
@@ -6,9 +6,11 @@ int main()
 
     //nullptr == 0
     //주소를 가리키지 않는다, 아직 아무도 가리키고 잇지 않다.
+    //보통은 nullptr로 초기화
     int* Ptr = nullptr;
 
-    *Ptr = 200;
+    //넣어줄 주소값이 있으면 바로 넣어줌.
+    int* Ptr1 = &Value;
 
     //int* Ptr = 0;     <= c스타일
 }
