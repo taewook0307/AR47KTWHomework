ad7f1fe55538fa86012c69bdfb84da9b428f19a2
TaeWook <ktwisland111@gmail.com>
2023-03-27 15:53:00 +0900

    김태욱 : 업데이트

---
+++ KTW_Homework/052_ClassConst/052_ClassConst.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+
+class Player
+{
+public:
+    Player()
+         : Hp(10)
+        , Att(100)
+    {
+
+    }
+
+    // 멤버 함수 뒤에 const
+    // 이 함수를 호출해도 이 클래스를 통해서 만들어진 객체는 변화하지 않는다.
+
+    void PrintStatus() const
+    {
+        // 함수 뒤에 const를 붙이는 것 : Player* const this -> const Player* const this
+        printf_s("능력치 출력");
+    }
+
+    int GetHp() const
+    {
+        return Hp;
+    }
+
+    void SetHp(int _Value)
+    {
+        Hp = _Value;
+    }
+
+    int GetAtt() const
+    {
+        return Att;
+    }
+
+    void SetAtt(int _Value)
+    {
+        Att = _Value;
+    }
+
+protected:
+private:
+    int Hp;
+    int Att;
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
---
+++ KTW_Homework/053_GlobalValue/053_GlobalValue.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+// C++ 은 전역의 존재 때문에 객체지향 언어가 아님
+
+// static 멤버 변수
+
+class Player
+{
+public:
+    Player()
+    {
+        ++PlayerCount;
+    }
+
+    int GetAllPlayerCount()
+    {
+        return PlayerCount;
+    }
+
+private:
+    static int PlayerCount;
+};
+
+int Player::PlayerCount = 0;
+
+int main()
+{
+    Player NewPlayer0 = Player();
+    Player NewPlayer1 = Player();
+    Player NewPlayer2 = Player();
+
+
+    int a = 0;
+}
