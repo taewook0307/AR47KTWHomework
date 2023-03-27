31b1f871ff2b0bd1cd28b5793cf45f7c4ab603c3
TaeWook <ktwisland111@gmail.com>
2023-03-27 10:37:26 +0900

    김태욱 : 업데이트

--- KTW_Homework/Homework0324/Homework0324.cpp
+++ KTW_Homework/Homework0324/Homework0324.cpp
@@ -8,8 +8,8 @@
 // 안해도 되는 숙제
 // 숙제3 : 장애물 만들고 배치하기
 // 숙제4 : f를 누르면 폭탄이 설치되고 눈에 보여야 함.
-// ------------------------------------------------------------------------- 숙제 구현 
 // 
+// --------------------------------------------------------- 숙제4까지 완료
 // 숙제5 : 일정시간이 지나고 폭탄이 터지면서
 
 int main()
@@ -30,9 +30,9 @@ int main()
 	int PlayerY = ScreenYSize / 2;
 	int PlayerX = ScreenXSize / 2;
 
-	char LastClickButton = '0';
+	char PlayerDirection = 'L';
 
-#pragma region 장애물 설치
+#pragma region 장애물
 	for (int WallCount = 10; WallCount > 0; --WallCount)
 	{
 		int x = rand() % ScreenXSize;
@@ -57,6 +57,8 @@ int main()
 			printf_s("\n");
 		}
 
+		printf_s("\n플레이어가 보는 방향 : %c", PlayerDirection);
+
 		if (0 == _kbhit())		// 키를 누르지 않았다면
 		{
 			Sleep(200);
@@ -75,90 +77,114 @@ int main()
 			{
 				break;
 			}
+
+			else if (PlayerDirection != 'L')			// 플레이어가 좌측을 보고 있지 않을 경우
+			{
+				PlayerDirection = 'L';
+				break;
+			}
+
 			else
 			{
 				if (PlayerX == 0)						// 플레이어가 제일 좌측에 위치했을 경우
 				{
 					PlayerX = 0;
-					LastClickButton = 'a';
 				}
 				else
 				{
 					PlayerX -= 1;
 					Arr[PlayerY][PlayerX + 1] = 'a';
-					LastClickButton = 'a';
 				}
 				break;
 			}
+
 		case 'd':
 		case 'D':
 			if (Arr[PlayerY][PlayerX + 1] == '@' || Arr[PlayerY][PlayerX + 1] == 'X')		// 플레이어가 폭탄 혹은 장애물쪽으로 가려고 할 경우
 			{
 				break;
 			}
+
+			else if (PlayerDirection != 'R')			// 플레이어가 우측을 보고 있지 않을 경우
+			{
+				PlayerDirection = 'R';
+				break;
+			}
+
 			else
 			{
 				if (PlayerX == ScreenXSize - 1)			// 플레이어가 제일 우측에 위치했을 경우
 				{
 					PlayerX = ScreenXSize - 1;
-					LastClickButton = Ch;
 				}
 				else
 				{
 					PlayerX += 1;
 					Arr[PlayerY][PlayerX - 1] = 'a';
-					LastClickButton = Ch;
 				}
 				break;
 			}
+
 		case 's':
 		case 'S':
 			if (Arr[PlayerY + 1][PlayerX] == '@' || Arr[PlayerY + 1][PlayerX] == 'X')		// 플레이어가 폭탄 혹은 장애물쪽으로 가려고 할 경우
 			{
 				break;
 			}
+
+			else if (PlayerDirection != 'D')			// 플레이어가 아래측을 보고 있지 않을 경우
+			{
+				PlayerDirection = 'D';
+				break;
+			}
+
 			else
 			{
 				if (PlayerY == ScreenYSize - 1)			// 플레이어가 제일 아래쪽에 위치했을 경우
 				{
 					PlayerY = ScreenYSize - 1;
-					LastClickButton = Ch;
 				}
 				else
 				{
 					PlayerY += 1;
 					Arr[PlayerY - 1][PlayerX] = 'a';
-					LastClickButton = Ch;
 				}
 				break;
 			}
+
 		case 'w':
 		case 'W':
 			if (Arr[PlayerY - 1][PlayerX] == '@' || Arr[PlayerY - 1][PlayerX] == 'X')		// 플레이어가 폭탄 혹은 장애물쪽으로 가려고 할 경우
 			{
 				break;
 			}
+
+			else if (PlayerDirection != 'U')			// 플레이어가 위쪽을 보고 있지 않을 경우
+			{
+				PlayerDirection = 'U';
+				break;
+			}
+
 			else
 			{
 
 				if (PlayerY == 0)						// 플레이어가 제일 위쪽에 위치했을 경우
 				{
 					PlayerY = 0;
-					LastClickButton = Ch;
 				}
 				else
 				{
 					PlayerY -= 1;
 					Arr[PlayerY + 1][PlayerX] = 'a';
-					LastClickButton = Ch;
 				}
 				break;
 			}
+
 		case 'f':										// 폭탄 설치
 		case 'F':
-			if (LastClickButton == 'a')					// 마지막으로 누른 키가 a일 경우
+			if (PlayerDirection == 'L')					// 플레이어가 좌측을 보고 있는 경우
 			{
-				if (PlayerX == 0)
+				if (PlayerX == 0)						// 플레이어가 맵에서 가장 좌측 라인에 있을 경우
 				{
 					break;
 				}
@@ -167,9 +193,10 @@ int main()
 					Arr[PlayerY][PlayerX - 1] = '@';
 				}
 			}
-			else if (LastClickButton == 'd')				// 마지막으로 누른 키가 d일 경우
+
+			else if (PlayerDirection == 'R')				// 플레이어가 우측을 보고 있는 경우
 			{
-				if (PlayerX == ScreenXSize - 1)
+				if (PlayerX == ScreenXSize - 1)			// 플레이어가 맵에서 가장 우측 라인에 있을 경우
 				{
 					break;
 				}
@@ -178,9 +205,10 @@ int main()
 					Arr[PlayerY][PlayerX + 1] = '@';
 				}
 			}
-			else if (LastClickButton == 's')			// 마지막으로 누른 키가 s일 경우
+
+			else if (PlayerDirection == 'D')			// 플레이어가 아래를 보고 있는 경우
 			{
-				if (PlayerY == ScreenYSize - 1)
+				if (PlayerY == ScreenYSize - 1)			// 플레이어가 맵에서 가장 아래쪽 라인에 있을 경우
 				{
 					break;
 				}
@@ -189,9 +217,10 @@ int main()
 					Arr[PlayerY + 1][PlayerX] = '@';
 				}
 			}
-			else if (LastClickButton == 'w')			// 마지막으로 누른 키가 w일 경우
+
+			else if (PlayerDirection == 'U')			// 플레이어가 위를 보고 있는 경우
 			{
-				if (PlayerY == 0)
+				if (PlayerY == 0)						// 플레이어가 맵에서 가장 위쪽 라인에 있을 경우
 				{
 					break;
 				}
