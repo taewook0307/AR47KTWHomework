19dfa3603be8facf80612416f385b156d99edb51
TaeWook <ktwisland111@gmail.com>
2023-04-13 20:45:03 +0900

    김태욱 : list, inner class, friend

--- KTW_Homework/074_Friend/074_Friend.cpp
+++ KTW_Homework/074_Friend/074_Friend.cpp
@@ -5,7 +5,7 @@ class B;
 class A
 {
 // friend 문법 : 특정 클래스에서 자신의 접근제한 지정자를 취소시키는 문법
-    friend B;
+    friend B;   // B에게는 자신 내부를 공개
 
 private:
     int Value = 0;
@@ -22,10 +22,17 @@ public:
 
 class C
 {
-
+    void Function(A& _Other)
+    {
+        // _Other.Value = 20;     C에서는 A의 Value가 사용 불가
+    }
 };
 
 int main()
 {
+    A NewA;
+    B NewB;
+    C NewC;
 
+    NewB.Function(NewA);
 }
