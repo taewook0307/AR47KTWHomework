e907171e31eddca879fbd7ff8cc3d77363c29c4a
TaeWook <ktwisland111@gmail.com>
2023-04-18 15:43:37 +0900

    김태욱 : 업데이트

---
+++ KTW_Homework/RValueRef/RValueRef.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+int Function()
+{
+    return 10;
+}
+
+int main()
+{
+    // 대입할 수 있고 메모리 영역을 특정할 수 있는 메모리들 : LValue
+    // 사용하거나 참조할 수만 있는 메모리들 : RValue
+    // LValue는 RValue가 될 수 있음
+    // RValue는 LValue가 될 수 없음
+
+    // & : LValue Reference => 명확하게 위치가 있고 메모리를 특정할 수 있어야 함
+    // && : RValue Reference => RValue를 받을 수 있음
+
+
+}
---
+++ KTW_Homework/StdString/StdString.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <vector>
+
+class MyString
+{
+private:
+    std::vector<char> Buffer;
+};
+
+int main()
+{
+    // 문자가 사용되는 대표적 예시
+    // 파일 입출력
+    // 파일 경로를 표현할 때
+
+    // 인코딩 : 멀티바이트, 와이드 바이트
+    // 1바이트 인코딩 : 아스키 or ANSI
+
+    // 1~2바이트 멀티 바이트 인코딩 <= 특정 한 국가의 문자정도는 표현이 가능
+    // 2바이트 와이드 바이트
+    // 2바이트 인코딩까지는 국가별로 달라지는 매칭형식
+
+    // 3바이트 문자형 및 4바이트 문자형 : UTF 인코딩 시리즈들
+
+    // wchar_t : 모든 문자가 2바이트
+
+    // std::string은 vector 형태의 자료구조
+    {
+        // char
+        std::string Text = "안녕하세요";
+
+        std::cout << Text << std::endl;
+    }
+
+    {
+        // 기본적으로 비쥬얼 스튜디오의 출력 국가 코드는 미국
+        // 와이드 바이트는 국가코드에 영향을 받음
+        
+        setlocale(LC_ALL, "KOR");       // 국가코드를 바꿔주는 함수
+
+        // wchar
+        std::wstring WText = L"반갑습니다";
+
+        std::wcout << WText << std::endl;
+    }
+
+    {
+        std::string Text = "안녕하세요";
+
+        // Text.replace()
+        // Text.find()
+        // Text.rfind()
+        // Text.find_last_not_of()
+        // Text.find_first_of()
+        // Text.append()
+        // 
+
+    }
+}
