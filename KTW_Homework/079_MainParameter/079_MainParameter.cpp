9eb325d342486d3b5f017a70707bba29210681af
TaeWook <ktwisland111@gmail.com>
2023-04-24 20:27:24 +0900

    김태욱 : 업데이트

---
+++ KTW_Homework/079_MainParameter/079_MainParameter.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+
+int main(int _Count, const char** _InitText)
+{
+    // 내 프로세스가 다른 프로세스를 실행할 수도 있다.
+    // system("");
+
+    // 프로그램이 실행되는 경로를 줌 : main의 인자
+}
---
+++ KTW_Homework/080_TypedefEx/080_TypedefEx.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+
+typedef int INT, * TEST;
+
+int main()
+{
+    INT a = 0;
+    TEST Ptr = &a;
+}
