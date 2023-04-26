a0a22fd3087c71bd01484e1cb5ab0b7b70be6813
TaeWook <ktwisland111@gmail.com>
2023-04-26 15:03:49 +0900

    김태욱 : 함수 호출 규약

---
+++ KTW_Homework/081_FunctionCallType/081_FunctionCallType.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+// 함수 호출 규약
+
+// 일반적인 전역함수 __cdecl
+void __cdecl Test()
+{
+
+}
+
+// 윈도우 함수에서 많이 사용
+void __stdcall Test()
+{
+
+}
+
+// 멤버 함수는 thiscall 이란 규약을 사용
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
