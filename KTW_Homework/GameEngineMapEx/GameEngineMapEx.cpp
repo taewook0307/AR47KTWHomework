45a5c5b04b1ec1d05a69cfbf549d862ec79b4a2c
Kim Tae Wook <gmill0411@naver.com>
2023-04-29 12:33:29 +0900

    김태욱 : map의 새로운 문법

--- KTW_Homework/GameEngineMapEx/GameEngineMapEx.cpp
+++ KTW_Homework/GameEngineMapEx/GameEngineMapEx.cpp
@@ -1,6 +1,44 @@
-﻿ #include <iostream>
+﻿#include <map>
+#include <string>
+
+template <typename KeyType, typename ValueType>
+class Pair
+{
+public:
+    KeyType first;
+    ValueType Second;
+};
+
+template <typename KeyType, typename ValueType>
+class MyMap
+{
+private:
+    class Node
+    {
+    private:
+        Pair<KeyType, ValueType> NewPair;
+    };
+
+public:
+    Node Root;
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::map<int, std::string> AllActor;
+
+    // AllActor의 first의 자료형 = int
+    // AllActor의 second의 자료형 = std::string
+
+    std::string& NewString = AllActor[0];
+
+    /* 33번줄 코드의 의미
+    std::map<int, std::string>::iterator FindIter = AllActor.find(0);
+
+    if (AllActor.end() == FindIter)
+    {
+        AllActor.insert(std::make_pair(0, "abc"));
+        FindIter = AllActor.find(0);
+    }
+    */
 }
