742f22f22dc6a67ed65f209d0bb9632266e15f8f
TaeWook <ktwisland111@gmail.com>
2023-05-16 15:52:13 +0900

    김태욱 : explicit

---
+++ KTW_Homework/088_Explicit/088_Explicit.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+class Test
+{
+public:
+    explicit Test(int Value)
+    {
+
+    }
+
+    void TestFunction()
+    {
+        std::cout << "Test Function" << std::endl;
+    }
+};
+
+int main()
+{
+    // Test NewTest = 10;           explicit이 걸어져 있어 약식으로 생성자를 호출하지 못 함
+    Test NewTest = Test(10);
+
+    NewTest.TestFunction();
+}
