57c944d109a8880665f6f3ba4b129b110a5ef35d
TaeWook <ktwisland111@gmail.com>
2023-05-25 15:23:09 +0900

    김태욱 : #ifdef, #endif

---
+++ KTW_Homework/089_Ifdef/089_Ifdef.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+// #define은 복사 붙여넣기
+// 
+#define TEST
+
+// TEST가 정의되어 있지 않으면
+#ifdef TEST
+
+//
+#else
+
+//
+#endif
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
